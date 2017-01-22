6cc0c632b6c6066744f691fc0883dcc4ec3afffc
fedora <fedora@fedhost.lan>
2017-01-22 11:46:59 +0100

    Added shell scripts

---
+++ shell_scripts/first_clone_from_github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Clona per la prima volta il repository da GitHub
+#Script creato da FXO in Gennaio 2017
+#versione 1
+
+git clone https://github.com/TheFax/monitor_solare
+cd ./monitor_solare
+chmod 755 solar_monitor.py
+
